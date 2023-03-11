0f167d4e73ce9f3f770225b04860fe039abff516
Sirawich <Sirawich_Pin@cmu.ac.th>
2023-03-11 23:06:28 +0700

    v1.4

--- src/Construction.jsx
+++ src/Construction.jsx
@@ -1,13 +1,24 @@
-function Construction() {
+import React from "react";
+
+const Construction = () => {
   return (
     <div>
-      <h1 style={{ margin: "0 auto", fontSize: "50px", padding: "10px 20px" }}>
+      <h1
+        style={{
+          margin: "0 auto",
+          fontSize: "50px",
+          padding: "10px 20px",
+          color: "#3E9DDE",
+        }}
+      >
         Build your Constrution plan
       </h1>
       <h1></h1>
-      <div></div>
+      <div>
+        <textarea type="text"></textarea>
+      </div>
     </div>
   );
-}
+};
 
 export default Construction;
